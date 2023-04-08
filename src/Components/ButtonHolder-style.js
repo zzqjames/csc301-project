ac6ad24649b6254f217ee6f884cd355adc1e6b70
zzqjames <90412404+zzqjames@users.noreply.github.com>
2023-04-08 08:12:16 -0400

    1

--- src/Components/ButtonHolder-style.js
+++ src/Components/ButtonHolder-style.js
@@ -1,26 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledButtonHolder = styled.div`
-    display: flex;
-    flex-direction: column;
-    > button {
-        
-        margin-bottom: 12px;
-        width: 30%;
-        height: 35px;
-        max-width: 90px;
-        border-radius: 8px;
-        border-style: solid;
-        background-color: #3CA4FF;
-        border-color: rgba(195, 82, 82, 0.888);
-        align-self: center;
-        font-weight: 700;
-        color: rgba(195, 82, 82, 0.888);
-    }
-
-    .button-holder button:hover {
-        cursor:pointer;
-        background-color: rgba(195, 82, 82, 0.888);
-        color: white;
-    }
-` 
+  display: flex;
+  flex-direction: column;
+  > button {
+    margin-bottom: 12px;
+    width: 30%;
+    height: 35px;
+    max-width: 90px;
+    border-radius: 8px;
+    border-style: solid;
+    background-color: #3ca4ff;
+    border-color: #3ca4ff;
+    align-self: center;
+    font-weight: 700;
+    color: white;
+  }
+  .button-holder button:hover {
+    cursor: pointer;
+    background-color: rgba(195, 82, 82, 0.888);
+    color: white;
+  }
+`;
--- src/Components/MainCard-style.js
+++ src/Components/MainCard-style.js
@@ -1,31 +1,29 @@
-import MainCard from './MainCard';
-import styled from 'styled-components';
+import MainCard from "./MainCard";
+import styled from "styled-components";
 
 export const StyledMainCard = styled(MainCard)`
-    background-color: #B5DAFA;
-    border-width: 3px;
-    color: black;
-
-    > h1 {
+  background-color: #b5dafa;
+  border-width: 3px;
+  color: black;
+  > h1 {
     font-size: 40px;
     font-weight: 400;
     font-family: "Quicksand";
-    margin: 0px 16px;
-    }
-
-    > p {
-        font-size: 18px;
-        font-weight: 200;
-        margin-top: 8px;
-        margin-left: 16px;
-        margin-bottom: 8px;
-        color: rgb(46, 42, 42);
-        font-family: "Quicksand";
-        text-decoration: underline;
-    }
-
-    > span {
-        display: flex;
-        flex-direction : column;
-    }
+    margin: 20px 20px 20px 20px;
+    margin-bottom: 50px;
+  }
+  > p {
+    font-size: 18px;
+    font-weight: 200;
+    margin-top: 8px;
+    margin-left: 16px;
+    margin-bottom: 8px;
+    color: rgb(46, 42, 42);
+    font-family: "Quicksand";
+    text-decoration: underline;
+  }
+  > span {
+    display: flex;
+    flex-direction: column;
+  }
 `;
